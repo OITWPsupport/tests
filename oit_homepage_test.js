45a38e1248f866b28f676da22215a84cdce60b15
davidmlentz <davidmlentz@gmail.com>
2017-07-25 12:39:57 -0600

    Updating oit test and adding after tests for webguide

--- oit_homepage_test.js
+++ oit_homepage_test.js
@@ -96,7 +96,7 @@ driver.findElements({className: 'post-footer'}).then(function(elements) {
         if (isPresent){
                 console.log('Page element of class \'post-footer\' is here');
 		require('./includes/footer.js').testFooter(webdriver, driver);
-		require('./includes/footer.js').testPhoneNumber(webdriver, driver, '208.426.4357');
+		require('./includes/footer.js').testPhoneNumber(webdriver, driver, '(208) 426-4357');
 		require('./includes/footer.js').testDeptName(webdriver, driver, 'OFFICE OF INFORMATION TECHNOLOGY');
 		require('./includes/footer.js').testEmail(webdriver, driver, 'HELPDESK@BOISESTATE.EDU');
 		require('./includes/footer.js').testMailingAddress(webdriver, driver, '1910 UNIVERSITY DRIVE');
