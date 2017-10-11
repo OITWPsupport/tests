dfb137e72b9e0d36198b688fff04af21db844253
David M. Lentz <davidmlentz@gmail.com>
2017-10-11 16:31:22 -0600

    Moving wdio.conf.js up one directory

--- webdriverio/wdio.conf.js
+++ webdriverio/wdio.conf.js
@@ -10,51 +10,34 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        '/Users/david/Documents/git_repos/tests/webdriverio/**/*.js'
+        './**/*.js'
     ],
     // Patterns to exclude.
     exclude: [
         // 'path/to/excluded/files'
     ],
     suites: {
-	aae: [
-		'/Users/david/Documents/git_repos/tests/webdriverio/aae/aae_can_search.js',
-		'/Users/david/Documents/git_repos/tests/webdriverio/aae/aae_has_search_field.js',
-		'/Users/david/Documents/git_repos/tests/webdriverio/aae/aae_nav_home.js',
-		'/Users/david/Documents/git_repos/tests/webdriverio/aae/aae_has_megamenu.js',
-		'/Users/david/Documents/git_repos/tests/webdriverio/aae/aae_has_social.js',
-		'/Users/david/Documents/git_repos/tests/webdriverio/aae/aae_title.js',
-		'/Users/david/Documents/git_repos/tests/webdriverio/aae/aae_footer.js'
-	],
-	ctl: [
-		'/Users/davidlentz/Documents/git_repos/tests/webdriverio/ctl/bug_test1.js'
-	],
-	academics: [
-		'/Users/davidlentz/Documents/git_repos/tests/webdriverio/academics/academics_can_search.js',
-		'/Users/davidlentz/Documents/git_repos/tests/webdriverio/academics/academics_has_search_field.js',
-		'/Users/davidlentz/Documents/git_repos/tests/webdriverio/academics/academics_nav_home.js', 
-		'/Users/davidlentz/Documents/git_repos/tests/webdriverio/academics/academics_has_megamenu.js',
-		'/Users/davidlentz/Documents/git_repos/tests/webdriverio/academics/academics_has_social.js',
-		'/Users/davidlentz/Documents/git_repos/tests/webdriverio/academics/academics_title.js'
-	],
-	admissions: [
-		'/Users/davidlentz/Documents/git_repos/tests/webdriverio/admissions/admissions_can_search.js',
-		'/Users/davidlentz/Documents/git_repos/tests/webdriverio/admissions/admissions_has_search_field.js',
-		'/Users/davidlentz/Documents/git_repos/tests/webdriverio/admissions/admissions_nav_home.js',
-		'/Users/davidlentz/Documents/git_repos/tests/webdriverio/admissions/admissions_has_megamenu.js',
-		'/Users/davidlentz/Documents/git_repos/tests/webdriverio/admissions/admissions_has_social.js',
-		'/Users/davidlentz/Documents/git_repos/tests/webdriverio/admissions/admissions_title.js'
-	],
+        aae: [
+                './aae/aae_can_search.js',
+                './aae_has_search_field.js',
+                './aae/aae_nav_home.js',
+                './aae/aae_has_megamenu.js',
+                './aae/aae_has_social.js',
+                './aae/aae_title.js',
+                './aae/aae_footer.js'
+        ],
 	new: [
-		'/Users/david/Documents/git_repos/tests/webdriverio/new/pageloadtime.js',
-		'/Users/david/Documents/git_repos/tests/webdriverio/new/footer.js',
-		'/Users/david/Documents/git_repos/tests/webdriverio/new/title.js',
-		'/Users/david/Documents/git_repos/tests/webdriverio/new/megamenu.js',
-		'/Users/david/Documents/git_repos/tests/webdriverio/new/navhome.js',
-		'/Users/david/Documents/git_repos/tests/webdriverio/new/search.js'
+		'./new/search.js'
+
 	],
-	admin: [
-		'/Users/davidlentz/Documents/git_repos/tests/webdriverio/admin/listsites.js'
+	search: [
+		'./new/search_api2.js'
+	],
+	search_oit: [
+                './new/search_oit.js'
+	],
+	search_test: [
+		'./new/search_test.js'
 	]
     },
     //
@@ -85,8 +68,7 @@ exports.config = {
         // 5 instances get started at a time.
         maxInstances: 5,
         //
-        // browserName: 'firefox'
-	browserName: 'chrome'
+        browserName: 'chrome'
     }],
     //
     // ===================
@@ -117,7 +99,7 @@ exports.config = {
     baseUrl: 'http://localhost',
     //
     // Default timeout for all waitFor* commands.
-    waitforTimeout: 31000,
+    waitforTimeout: 10000,
     //
     // Default timeout in milliseconds for request
     // if Selenium Grid doesn't send response
@@ -160,18 +142,14 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: http://webdriver.io/guide/testrunner/reporters.html
-//    reporters: ['spec'],
-	reporters: ['dot'],
+    reporters: ['spec'],
     
     //
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
-
     mochaOpts: {
-        ui: 'bdd',
-        timeout: 31000
+        ui: 'bdd'
     },
-
     //
     // =====
     // Hooks
@@ -180,66 +158,105 @@ exports.config = {
     // it and to build services around it. You can either apply a single function or an array of
     // methods to it. If one of them returns with a promise, WebdriverIO will wait until that promise got
     // resolved to continue.
-    //
-    // Gets executed once before all workers get launched.
+    /**
+     * Gets executed once before all workers get launched.
+     * @param {Object} config wdio configuration object
+     * @param {Array.<Object>} capabilities list of capabilities details
+     */
     // onPrepare: function (config, capabilities) {
     // },
-    //
-    // Gets executed just before initialising the webdriver session and test framework. It allows you
-    // to manipulate configurations depending on the capability or spec.
+    /**
+     * Gets executed just before initialising the webdriver session and test framework. It allows you
+     * to manipulate configurations depending on the capability or spec.
+     * @param {Object} config wdio configuration object
+     * @param {Array.<Object>} capabilities list of capabilities details
+     * @param {Array.<String>} specs List of spec file paths that are to be run
+     */
     // beforeSession: function (config, capabilities, specs) {
     // },
-    //
-    // Gets executed before test execution begins. At this point you can access all global
-    // variables, such as `browser`. It is the perfect place to define custom commands.
+    /**
+     * Gets executed before test execution begins. At this point you can access to all global
+     * variables like `browser`. It is the perfect place to define custom commands.
+     * @param {Array.<Object>} capabilities list of capabilities details
+     * @param {Array.<String>} specs List of spec file paths that are to be run
+     */
     // before: function (capabilities, specs) {
     // },
     //
-    // Hook that gets executed before the suite starts
+    /**
+     * Hook that gets executed before the suite starts
+     * @param {Object} suite suite details
+     */
     // beforeSuite: function (suite) {
     // },
-    //
-    // Hook that gets executed _before_ a hook within the suite starts (e.g. runs before calling
-    // beforeEach in Mocha)
+    /**
+     * Hook that gets executed _before_ a hook within the suite starts (e.g. runs before calling
+     * beforeEach in Mocha)
+     */
     // beforeHook: function () {
     // },
-    //
-    // Hook that gets executed _after_ a hook within the suite starts (e.g. runs after calling
-    // afterEach in Mocha)
+    /**
+     * Hook that gets executed _after_ a hook within the suite starts (e.g. runs after calling
+     * afterEach in Mocha)
+     */
     // afterHook: function () {
     // },
-    //
-    // Function to be executed before a test (in Mocha/Jasmine) or a step (in Cucumber) starts.
+    /**
+     * Function to be executed before a test (in Mocha/Jasmine) or a step (in Cucumber) starts.
+     * @param {Object} test test details
+     */
     // beforeTest: function (test) {
     // },
-    //
-    // Runs before a WebdriverIO command gets executed.
+    /**
+     * Runs before a WebdriverIO command gets executed.
+     * @param {String} commandName hook command name
+     * @param {Array} args arguments that command would receive
+     */
     // beforeCommand: function (commandName, args) {
     // },
-    //
-    // Runs after a WebdriverIO command gets executed
+    /**
+     * Runs after a WebdriverIO command gets executed
+     * @param {String} commandName hook command name
+     * @param {Array} args arguments that command would receive
+     * @param {Number} result 0 - command success, 1 - command error
+     * @param {Object} error error object if any
+     */
     // afterCommand: function (commandName, args, result, error) {
     // },
-    //
-    // Function to be executed after a test (in Mocha/Jasmine) or a step (in Cucumber) starts.
+    /**
+     * Function to be executed after a test (in Mocha/Jasmine) or a step (in Cucumber) starts.
+     * @param {Object} test test details
+     */
     // afterTest: function (test) {
     // },
-    //
-    // Hook that gets executed after the suite has ended
+    /**
+     * Hook that gets executed after the suite has ended
+     * @param {Object} suite suite details
+     */
     // afterSuite: function (suite) {
     // },
-    //
-    // Gets executed after all tests are done. You still have access to all global variables from
-    // the test.
+    /**
+     * Gets executed after all tests are done. You still have access to all global variables from
+     * the test.
+     * @param {Number} result 0 - test pass, 1 - test fail
+     * @param {Array.<Object>} capabilities list of capabilities details
+     * @param {Array.<String>} specs List of spec file paths that ran
+     */
     // after: function (result, capabilities, specs) {
     // },
-    //
-    // Gets executed right after terminating the webdriver session.
+    /**
+     * Gets executed right after terminating the webdriver session.
+     * @param {Object} config wdio configuration object
+     * @param {Array.<Object>} capabilities list of capabilities details
+     * @param {Array.<String>} specs List of spec file paths that ran
+     */
     // afterSession: function (config, capabilities, specs) {
     // },
-    //
-    // Gets executed after all workers got shut down and the process is about to exit. It is not
-    // possible to defer the end of the process using a promise.
+    /**
+     * Gets executed after all workers got shut down and the process is about to exit. It is not
+     * possible to defer the end of the process using a promise.
+     * @param {Object} exitCode 0 - success, 1 - fail
+     */
     // onComplete: function(exitCode) {
     // }
 }
